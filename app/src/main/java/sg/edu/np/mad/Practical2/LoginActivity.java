ac331f39895db460bb5aaa60d1a042cddce8f832
Brand <s10194173@connect.np.edu.sg>
2022-05-30 03:16:10 +0800

    .

--- app/src/main/java/sg/edu/np/mad/Practical2/LoginActivity.java
+++ app/src/main/java/sg/edu/np/mad/Practical2/LoginActivity.java
@@ -18,13 +18,17 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
     EditText loginUsername, loginPassword;
     Button loginButton;
     FirebaseAuth fAuth;
-    FirebaseDatabase fData;
+    DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,37 +39,78 @@ public class LoginActivity extends AppCompatActivity {
         loginPassword = findViewById(R.id.editTextNumberPassword);
         loginButton = findViewById(R.id.button5);
         fAuth = FirebaseAuth.getInstance();
-        fData = FirebaseDatabase.getInstance();
+
+
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String username = loginUsername.getText().toString().trim();
-                String password = loginPassword.getText().toString().trim();
-
-                if(TextUtils.isEmpty(username)){
-                    loginUsername.setError("Username is required");
-                }
-
-                if(TextUtils.isEmpty(password)){
-                    loginPassword.setError("Password is required");
-                }
+            public void onClick(View view) {
+                String username = loginUsername.getText().toString();
+                String password = loginPassword.getText().toString();
 
 
-                // authenticate the user
-                fAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                ref = FirebaseDatabase.getInstance("https://practical6-a6bdc-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("Users");
+                Query usernameCheck = ref.orderByChild("username").equalTo(username);
+                Query passwordCheck = ref.orderByChild("password").equalTo(password);
+                usernameCheck.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        String test;
-                        if(task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this, "Logged in successfully",Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), ListActivity.class));
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.exists()){
+                            passwordCheck.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    if(snapshot.exists()){
+                                        startActivity(new Intent(getApplicationContext(), ListActivity.class));
+                                    }
+                                    else{
+                                        Toast.makeText(LoginActivity.this, "Wrong password",Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+                            //String passwordfromDb = snapshot.child(password).child("password").getValue(String.class);
                         }
                         else{
-                            Toast.makeText(LoginActivity.this, "Credentials are wrong, please try again",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "User does not exist",Toast.LENGTH_SHORT).show();
                         }
                     }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
                 });
+
+
+                //if(TextUtils.isEmpty(username)){
+                //    loginUsername.setError("Username is required");
+                //}
+
+                //if(TextUtils.isEmpty(password)){
+                //    loginPassword.setError("Password is required");
+                //}
+
+                // authenticate the user
+                //fAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                //    @Override
+                //    public void onComplete(@NonNull Task<AuthResult> task) {
+                //        String test;
+                //        if(task.isSuccessful()){
+                //            Toast.makeText(LoginActivity.this, "Logged in successfully",Toast.LENGTH_SHORT).show();
+                //            startActivity(new Intent(getApplicationContext(), ListActivity.class));
+                //        }
+                //        else{
+                //            Toast.makeText(LoginActivity.this, "Credentials are wrong, please try again",Toast.LENGTH_SHORT).show();
+                //        }
+                //    }
+                //});
+
+
 
 
             }
